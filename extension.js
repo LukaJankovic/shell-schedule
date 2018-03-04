42156bb6d00fab619962558f3cfa64d2369cd4ec
Luka Jankovic <lukjan1999@gmail.com>
2018-03-04 22:56:20 +0100

    shows local image

--- extension.js
+++ extension.js
@@ -5,6 +5,10 @@ const Lang = imports.lang;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const GdkPixbuf = imports.gi.GdkPixbuf;
+const Clutter = imports.gi.Clutter;
+const Cogl = imports.gi.Cogl;
 
 const ScheduleIndicator = new Lang.Class({
     Name: "ScheduleIndicator",
@@ -17,7 +21,25 @@ const ScheduleIndicator = new Lang.Class({
 
         this.icon = new St.Icon({ gicon: gicon, style_class: 'system-status-icon' });
         this.actor.add_child(this.icon);
-    }
+
+        let image_item = new PopupMenu.PopupBaseMenuItem({can_focus: false, reactive: false});
+        this.menu.addMenuItem(image_item);
+
+        let image = new Clutter.Image();
+        let pixbuf = GdkPixbuf.Pixbuf.new_from_file("/home/luka/Pictures/schedulegenerator.png");
+
+        let image_actor = new Clutter.Actor({height: 600, width: 480});
+        image_actor.set_content(image);
+        image.set_data(
+                      pixbuf.get_pixels(),
+                      pixbuf.get_has_alpha() ? Cogl.PixelFormat.RGBA_8888 : Cogl.PixelFormat.RGB_888,
+                      480,
+                      600,
+                      pixbuf.get_rowstride()
+                      );
+
+        image_item.actor.add_actor(image_actor)
+    },
 })
 
 let schedule_indicator;
