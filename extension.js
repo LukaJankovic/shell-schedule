a69fbab76fafad693f21580686b81bbd4f28bb72
LukaJankovic <lukjan1999@gmail.com>
2018-04-15 22:44:23 +0200

    redone image downloading

--- extension.js
+++ extension.js
@@ -14,6 +14,11 @@ const Soup = imports.gi.Soup;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Convenience = Me.imports.convenience;
 
+let schedule_indicator;
+let parent_container;
+let image_container;
+let current_hidpi;
+
 function getWeekNumber() {
     var d = new Date();
     d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
@@ -21,11 +26,6 @@ function getWeekNumber() {
     var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1)/7);
     return weekNo;
 }
-
-let schedule_indicator;
-let parent_container;
-let image_container;
-let current_hidpi;
 
 function updateHiDPiIfNeeded() {
 	if (current_hidpi != St.ThemeContext.get_for_stage(global.stage).scale_factor) {
@@ -47,36 +47,26 @@ function updateHiDPiIfNeeded() {
 function loadSchedule() {
     var _schema = Convenience.getSettings();
 
-    //Delete old schedule file
-    try {
-        let f = Gio.File.new_for_path(Me.path + '/schedule.png');
-        f.delete(Gio.Cancellable.new());
-    } catch(e) {
-        global.log("no schedule file to delete");
-    }
-
     updateHiDPiIfNeeded();
-       
+
     let session = new Soup.SessionAsync();
     Soup.Session.prototype.add_feature.call(session, new Soup.ProxyResolverDefault());
-    
+
     let classID = _schema.get_string("classid");
     let schoolID = _schema.get_string("schoolid");
 
     var image = new Clutter.Image();
-    
+
     const URL = "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?format=png&schoolid="+schoolID+"/sv-se&id="+classID+"&period=&week="+getWeekNumber()+"&colors=32&day=0&width=480&height=600";
-    
+
     let request = Soup.Message.new_from_uri('GET', new Soup.URI(URL));
     session.queue_message(request, ((session, message) => {
 	
         if (message.status_code == 200) {
-            let file = Gio.File.new_for_path(Me.path + '/schedule.png');
-            let outstream = file.replace(null, false, Gio.FileCreateFlags.NONE,null);
-            outstream.write_bytes(message.response_body.flatten().get_as_bytes(),null);
-	    
-            let pixbuf = GdkPixbuf.Pixbuf.new_from_file(Me.path + '/schedule.png');
-	    
+
+	    	let inputStream = Gio.MemoryInputStream.new_from_bytes(message.response_body_data)
+	    	let pixbuf = GdkPixbuf.Pixbuf.new_from_stream(inputStream, null);
+
             image.set_data(
                 pixbuf.get_pixels(),
                 pixbuf.get_has_alpha() ? Cogl.PixelFormat.RGBA_8888 : Cogl.PixelFormat.RGB_888,
@@ -84,7 +74,7 @@ function loadSchedule() {
                 600,
                 pixbuf.get_rowstride()
             );
-	    
+
             image_container.set_content(image);
         }
     }));
